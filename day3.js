6f83bef2e4c0abedf0f2469a137b63caf77b2f61
Saqibqazi7 <160391573+Saqibqazi7@users.noreply.github.com>
2024-03-19 01:37:41 +0500

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,19 @@
+//Subtraction
+/*let num1 = 12
+let num2 = 4
+console.log(num1 - num2)
+console.log(20 - 12);*/
+//Addition
+//console.log(4 + 4);
+//Mutiplicaion
+//console.log(2 * 4);
+//Divison
+//console.log(80 / 10);
+//Question no 2
+/*console.log(4 + 4);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(16 / 2);*/
+//Question no 3
+var favouritenumber = 7;
+console.log('My', favouritenumber, 'is', { favouritenumber: favouritenumber });
